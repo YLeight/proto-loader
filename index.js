3782757373fcc786d2bd8e73921053700bb53086
Guro Bokum <jiojiajiu@gmail.com>
2017-07-02 15:49:36 +0300

    Change \t on spaces

--- index.js
+++ index.js
@@ -8,51 +8,51 @@ var path = require("path");
 var loaderUtils = require('loader-utils');
 
 module.exports = function (source) {
-	var options = loaderUtils.parseQuery(this.query) || {};
-	var self = this;
+  var options = loaderUtils.parseQuery(this.query) || {};
+  var self = this;
 
-	function readProto(src, options, loaded) {
-		var parser = new ProtoBuf.DotProto.Parser(src),
-			data = parser.parse();
+  function readProto(src, options, loaded) {
+    var parser = new ProtoBuf.DotProto.Parser(src),
+      data = parser.parse();
 
-		if (!loaded) {
-			loaded = {};
-		}
+    if (!loaded) {
+      loaded = {};
+    }
 
-		if (Array.isArray(data.imports)) {
-			data.imports.forEach(function (imp, index) {
-				var i,
-					fileName;
+    if (Array.isArray(data.imports)) {
+      data.imports.forEach(function (imp, index) {
+        var i,
+          fileName;
 
-				// Skip pulled imports and legacy descriptors
-				if (typeof imp !== 'string' || (protoUtil.isDescriptor(imp) && !options.legacy)) {
-					return;
-				}
+        // Skip pulled imports and legacy descriptors
+        if (typeof imp !== 'string' || (protoUtil.isDescriptor(imp) && !options.legacy)) {
+          return;
+        }
 
-				fileName = path.resolve(self.context + '/' + imp);
-				if (fs.existsSync(fileName)) {
-					if (loaded[fileName]) {
-						data.imports[index] = {};
-						return;
-					}
-					loaded[fileName] = true;
-					data.imports[index] = readProto(fs.readFileSync(fileName).toString('utf8'));
-					return;
-				}
-				throw Error('File not found: ' + imp);
-			});
-		}
+        fileName = path.resolve(self.context + '/' + imp);
+        if (fs.existsSync(fileName)) {
+          if (loaded[fileName]) {
+            data.imports[index] = {};
+            return;
+          }
+          loaded[fileName] = true;
+          data.imports[index] = readProto(fs.readFileSync(fileName).toString('utf8'));
+          return;
+        }
+        throw Error('File not found: ' + imp);
+      });
+    }
 
-		return data;
-	}
+    return data;
+  }
 
-	if (this.cacheable()) {
-		this.cacheable();
-	}
+  if (this.cacheable()) {
+    this.cacheable();
+  }
 
-	var data = readProto(source, options);
-	var builder = ProtoBuf.newBuilder(protoUtil.getBuilderOptions(options, 'using'));
-	builder['import'](data);
+  var data = readProto(source, options);
+  var builder = ProtoBuf.newBuilder(protoUtil.getBuilderOptions(options, 'using'));
+  builder['import'](data);
 
-	return 'module.exports = ' + protoTarget(builder, options) + ';';
+  return 'module.exports = ' + protoTarget(builder, options) + ';';
 };
