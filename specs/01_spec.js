58d2bb7939fbc5fe7d742414778c6b0aec3fb381
ylitvyakov <ylitvyakov@yahoo.com>
2018-01-23 05:29:11 +0300

    specs: add first spec

---
+++ specs/01_spec.js
@@ -0,0 +1,9 @@
+describe('First spec description:', function () {
+  it('first case in spec', function () {
+    expect(true).toBe(true);
+  });
+  it('second case in spec', function () {
+    expect(true).toBe(false);
+  });
+});
+
